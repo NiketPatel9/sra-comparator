5e80dfb9fdbfcc91c2371d6d9d6cdb79543affaf
NiketPatel9 <niketp14@gmail.com>
2021-05-02 19:37:17 -0400

    finished star pipeline

--- star_pipeline.sh
+++ star_pipeline.sh
@@ -6,3 +6,10 @@
 ./star/starAlign.sh
 ./samTools.sh
 ./mergeBam.sh
+./assemble_transcriptome.sh
+./assemblyStats.sh > STAROUTPUT.txt
+rm -rf ./GenomeIndices
+rm -rf ./trinity_out_dir
+rm -rf ./sam
+rm -rf ./bam
+rm -rf ./sortedBam
